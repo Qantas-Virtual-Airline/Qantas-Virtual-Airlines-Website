2144b39228259fcde7d782ada2b0b86e76e7062f
H0ck3y-QANTAS <fleishmans708@mysaddleback.org>
2026-02-10 14:25:40 -0800

    Create navbar.js

---
+++ src/ui/navbar.js
@@ -0,0 +1,12 @@
+export function renderNavbar() {
+  return `
+    <nav class="nav">
+      <a href="/pages/index.html">Home</a>
+      <a href="/pages/map.html">Map</a>
+      <a href="/pages/dashboard.html">Dashboard</a>
+      <a href="/pages/apply.html">Apply</a>
+      <a href="/pages/staff.html">Staff</a>
+      <button id="loginBtn">Login</button>
+    </nav>
+  `;
+}
